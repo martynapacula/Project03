5bf2e164a2c46490fac088d3c30aa70d3ae847f9
martynapacula <martynapacula@gmail.com>
2020-03-01 15:21:57 +0000

    connected MongodB tasks to app.py with correct task names

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.cinfig["MONGO_URI"] = 'mongodb+srv://first_user14:<password>F1rstUser@project03-4ieku.mongodb.net/task_manager?retryWrites=true&w=majority
-'
-mongo - PyMongo(app)
+app.config["MONGO_URI"] = 'mongodb+srv://first_user14:F1rstUser@project03-4ieku.mongodb.net/task_manager?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 
 @app.route('/')
