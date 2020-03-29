68fe4aac2a751407e7729378418b2f66ed5ce5a1
martynapacula <martynapacula@gmail.com>
2020-03-29 13:45:31 +0000

    test background colour to red for base.html

--- app.py
+++ app.py
@@ -14,18 +14,19 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html",
-    tasks=mongo.db.tasks.find())
+                           tasks=mongo.db.tasks.find())
 
 
 
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html',
-    categories=mongo.db.categories.find())
+                           categories=mongo.db.categories.find())
+
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks = mogo.db.tasks
+    tasks=mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
