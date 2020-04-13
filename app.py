dcbc1df2c922bc29cb857b494f0bb17e0c04e1fb
martynapacula <martynapacula@gmail.com>
2020-04-13 10:45:11 +0000

    updateing the category function in app.py file

--- app.py
+++ app.py
@@ -70,6 +70,12 @@ def edit_category(category_id):
     return render_template('editcategory.html',
     category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
 
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
 
 
 if __name__ == '__main__':
