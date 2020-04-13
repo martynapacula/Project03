e222f4580ac1460133893a20332433f69c36698b
martynapacula <martynapacula@gmail.com>
2020-04-13 13:55:57 +0000

    delete functionality to categories form added

--- app.py
+++ app.py
@@ -74,9 +74,13 @@ def edit_category(category_id):
 def update_category(category_id):
     mongo.db.categories.update(
         {'_id': ObjectId(category_id)},
-        {'category_name': request.form.get('category_name')})
+        {'Category_name': request.form.get('Category_name')})
     return redirect(url_for('get_categories'))
 
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.category.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
