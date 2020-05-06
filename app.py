8b81291ab1b0de72a68c3a0f188500eb755cfc7b
martynapacula <martynapacula@gmail.com>
2020-05-06 18:58:05 +0000

    functions naming changed to match the project description in every file

--- app.py
+++ app.py
@@ -17,29 +17,29 @@ def get_experiment():
                            tasks=mongo.db.tasks.find())
 
 
-@app.route('/add_task')
-def add_task():
+@app.route('/add_experiment')
+def add_experiment():
     return render_template('addtask.html',
                            categories=mongo.db.categories.find())
 
 
-@app.route('/insert_task', methods=['POST'])
-def insert_task():
+@app.route('/insert_experiment', methods=['POST'])
+def insert_experiment():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
-    return redirect(url_for('get_tasks'))
+    return redirect(url_for('get_experiment'))
 
 
-@app.route('/edit_task/<task_id>')
-def edit_task(task_id):
+@app.route('/edit_experiment/<task_id>')
+def edit_experiment(task_id):
     the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_categories = mongo.db.categories.find()
     return render_template('edittask.html', task=the_task,
                            categories=all_categories)
 
 
-@app.route('/update_task/<task_id>', methods=["POST"])
-def update_task(task_id):
+@app.route('/update_experiment/<task_id>', methods=["POST"])
+def update_experiment(task_id):
     tasks = mongo.db.tasks
     tasks.update({'_id': ObjectId(task_id)},
     {
@@ -53,46 +53,46 @@ def update_task(task_id):
     return redirect(url_for('get_experiment'))
 
 
-@app.route('/delete_task/<task_id>')
-def delete_task(task_id):
+@app.route('/delete_experiment/<task_id>')
+def delete_experiment(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_experiment'))
 
 
-@app.route('/get_categories')
-def get_categories():
+@app.route('/get_cat_experiment')
+def get_cat_experiment():
     return render_template('categories.html',
                            categories=mongo.db.categories.find())
                        
 
-@app.route('/edit_category/<category_id>')
-def edit_category(category_id):
+@app.route('/edit_cat_experiment/<category_id>')
+def edit_cat_experiment(category_id):
     return render_template('editcategory.html',
     category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
 
-@app.route('/update_category/<category_id>', methods=['POST'])
-def update_category(category_id):
+@app.route('/update_cat_experiment/<category_id>', methods=['POST'])
+def update_cat_experiment(category_id):
     mongo.db.categories.update(
         {'_id': ObjectId(category_id)},
         {'Category_name': request.form.get('Category_name')})
-    return redirect(url_for('get_categories'))
+    return redirect(url_for('get_cat_experiment'))
 
-@app.route('/delete_category/<category_id>')
-def delete_category(category_id):
+@app.route('/delete_cat_experiment/<category_id>')
+def delete_cat_experiment(category_id):
     mongo.db.categories.remove({'_id': ObjectId(category_id)})
-    return redirect(url_for('get_categories'))
+    return redirect(url_for('get_cat_experiment'))
 
 
     
-@app.route('/insert_category', methods=['POST'])
-def insert_category():
+@app.route('/insert_cat_experiment', methods=['POST'])
+def insert_cat_experiment():
     category_doc = {'Category_name': request.form.get('Category_name')}
     mongo.db.categories.insert_one(category_doc)
-    return redirect(url_for('get_categories'))
+    return redirect(url_for('get_cat_experiment'))
 
 
-@app.route('/add_category')
-def add_category():
+@app.route('/add_cat_experiment')
+def add_cat_experiment():
     return render_template('addcategory.html')
 
 
